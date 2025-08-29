aa84fc69fff5d93d43a9876abbd32fab49de8fd8
Roberto Méndez Méndez <91458910+RobertoMendezM@users.noreply.github.com>
2025-08-29 05:00:33 -0600

    Caracteres_como Enteros.c

--- Calculo_Numerico_Basico/Caracteres_como Enteros.c
+++ Calculo_Numerico_Basico/Caracteres_como Enteros.c
@@ -1,8 +1,7 @@
 /********************************************************************
-Caracteres como Enteros (ASCII)
+MAnejo de Caracteres como Enteros (ASCII) en C
 
-
-Curso: Matemáticas para Ciencias Aplicadas I
+Curso: Matemáticas para Ciencias Aplicadas I 2026-1
 Autor:   Roberto Méndez
 Edición: 27 Ago 2025
 ********************************************************************/
@@ -10,13 +9,14 @@ Edición: 27 Ago 2025
 #include <stdio.h>
 
 int main()
-
 {
    char a, b;
    int suma;
     printf("Dame dos letras separadas por un espacio: ");
-    scanf("%c %c", &a, & b);
+    scanf("%c %c", &a, &b);
     suma = a + b;
+    printf("%c en ASCII vale %d", a, a);
+    printf("%c en ASCII vale %d", b, b);
     printf("La suma de %c + %c es: %d", a, b, suma);
     return 0;
 }
