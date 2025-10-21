1f52144122d342bf5700fb4f9a5cb23bdc36ccb2
Roberto Méndez Méndez <91458910+RobertoMendezM@users.noreply.github.com>
2025-10-21 12:32:36 -0600

    Add files via upload

---
+++ Sucesiones/Fibonacci_Recursivo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Fibonacci recursivo
+
+Tema: Sobrecarga del proceso por saturación de llamadas recursivas
+
+
+Notas: Forma totalmente incorrecta de programar fibonacci
+
+ Función: 
+     fibRec(n: int ) -> int:
+         Calculates the nth Fibonacci number.
+         Args  n: The index of the Fibonacci number to calculate.
+         Returns: The nth Fibonacci number.
+
+Software: Python 3.12.12
+
+@author: Roberto Méndez
+Created on Thu Jun 13 10:13:22 2024
+Editado: 21 Oct 2025
+"""
+
+def fibRec(n: int ) -> int:
+  if n <= 0 or n == 1:
+    return n
+  else:
+    return fibRec(n-1) + fibRec(n-2)
+
+n = int(input("Da el índice de valor de Fibonacci que deseas: "))
+
+# Calculate and print the Fibonacci number
+print(f"The {n}th Fibonacci number is: {fibRec(n)}")
---
+++ Sucesiones/fibonacci_MemDic1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Fibonacci con Memoria con cálculo hacia abajo
+
+Tema: Sucesión con Memoria para evitar llamadas redundantes
+   
+   Dic[int, int] represents a dictionary where both keys and values are 
+       integers. This means that each key in the dictionary must be an integer, 
+       and the value associated with each key must also be an integer.
+      
+      En el diccionario se guardan las evaluaciones de la sucesión de fibonacci
+      (aún cuando no se hayan evaluado explicitamente en un inico), con lo cual 
+      se evita la llamada recurrente a valores ya "calculados".
+   
+   if __name__ == "__main__":
+      Sirve para ejecutar un bloque de código solo cuando el script se 
+      ejecuta directamente, y no cuando se importa como un módulo en otro 
+      archivo.
+
+
+Referencia: Classic Computer Science Problems with Python
+            pag 10   fib3.py 
+
+
+Software: Python 3.12.12
+
+Editor: Roberto Méndez Méndez
+
+Created on Fri Jun 14 2024
+Edited: Oct 30 2025
+"""
+
+from typing import Dict
+
+sucFib: Dict[int, int] = {0: 0, 1: 1}
+
+def fibMem(n : int) -> int:
+    if n not in sucFib:
+        sucFib[n] = fibMem(n-1) + fibMem(n-2)
+    return sucFib[n]
+
+if __name__ == "__main__":
+    n = int(input("¿Fibonacci en la posición?: "))
+    print(f"The {n}th Fibonacci number is: {fibMem(n)}")
+    print("Los valores calculados de la sucesión son: ")
+    for z in sucFib:
+        print(f"{z} : {sucFib[z]}")
+        
