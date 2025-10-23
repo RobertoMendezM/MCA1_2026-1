6461d71357dc5feda87bb20e0d2e88f66ed625f3
Roberto Méndez Méndez <91458910+RobertoMendezM@users.noreply.github.com>
2025-10-23 04:53:27 -0600

    Add files via upload

--- Sucesiones/Fibonacci_Recursivo.py
+++ Sucesiones/Fibonacci_Recursivo.py
@@ -13,15 +13,18 @@ Notas: Forma totalmente incorrecta de programar fibonacci
          Args  n: The index of the Fibonacci number to calculate.
          Returns: The nth Fibonacci number.
 
+Referencia: Classic Computer Science Problems with Python
+            pag 8   fib2.py 
+
 Software: Python 3.12.12
 
-@author: Roberto Méndez
+editor: Roberto Méndez
 Created on Thu Jun 13 10:13:22 2024
-Editado: 21 Oct 2025
+Editado: 22 Oct 2025
 """
-
+ 
 def fibRec(n: int ) -> int:
-  if n <= 0 or n == 1:
+  if n == 0 or n == 1:
     return n
   else:
     return fibRec(n-1) + fibRec(n-2)
@@ -30,3 +33,7 @@ n = int(input("Da el índice de valor de Fibonacci que deseas: "))
 
 # Calculate and print the Fibonacci number
 print(f"The {n}th Fibonacci number is: {fibRec(n)}")
+
+
+
+
