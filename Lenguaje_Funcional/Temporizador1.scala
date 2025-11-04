878fca271ad762aec0c71fdd53e8d1dac8a03bb7
Roberto Méndez Méndez <91458910+RobertoMendezM@users.noreply.github.com>
2025-11-04 05:46:30 -0600

    Add files via upload

--- Lenguaje_Funcional/Temporizador1.scala
+++ Lenguaje_Funcional/Temporizador1.scala
@@ -3,11 +3,14 @@
  *
  * Curso: MCA 1 2026-1 FCiencias
  *
- * tema: Pasod de una función
+ * tema: Paso de una función como parámetro
  *       Thread
  *
  * Referencia:
  * https://docs.scala-lang.org/es/tutorials/scala-for-java-programmers.html
+ * https://docs.scala-lang.org/scala3/book/control-structures.html#the-ifthenelse-construct
+ *
+ * Software: Scala 3.7.3
  *
  * Editor: Roberto Méndez
  * Credo: 4 Nov 25
@@ -15,9 +18,13 @@
 
 object Temporizador1 {
   def unaVezPorSegundo(fcallback: () => Unit): Unit = {
-    while(true){
+    var n = 0
+    var continue = true
+    while(continue){
       fcallback();
       Thread sleep 1000
+      if n == 6 then continue = false
+      n += 1
     }
   }
 
